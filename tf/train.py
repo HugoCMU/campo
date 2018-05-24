194d2342ea6be17e378f3f0a237d85e8f7657fab
ook-yoga <hugo.p.cmu@gmail.com>
2018-05-24 15:58:45 -0700

    regex for plant age

--- tf/train.py
+++ tf/train.py
@@ -1,13 +1,16 @@
+import datetime
 from pathlib import Path
 import re
 import tensorflow as tf
 import tensorflow_hub as hub
 
 # Load datasets (USE tf.data API)
-DATASET_NAME = 'bleh'
 root_dir = Path.cwd()
-train_dir = root_dir / 'data' / DATASET_NAME / 'train'
-test_dir = root_dir / 'data' / DATASET_NAME / 'test'
+data_dir = root_dir / 'data' / 'images_and_annotations'
+train_dir = data_dir / 'PSI_Tray031' / 'tv'
+test_dir = data_dir / 'PSI_Tray032' / 'tv'
+PLANT_START_DATE = datetime.datetime(2015, 12, 14, hour=12, minute=54, second=51)
+PLANT_AGE_MULT = 1.5 * 2592000 # Age of plant in seconds for normalizing
 
 # feature encoder is taken from tf hub
 image_features = hub.image_embedding_column('plant-photo',
@@ -29,8 +32,13 @@ def parse_single_image(filename, train=True):
         image = tf.image.random_contrast(image)
         image = tf.image.random_brightness(image)
     # TODO: Use regex to extract target from filename
-    filename_regex = re.search(r'(201\d)-(\d++)-(\d++)--(\d++)-(\d++)-(\d++)', filename).group()
-    date =
+    # Sample filename: PSI_Tray031_2015-12-26--17-38-25_top.png
+    filename_regex = re.search(r'(201\d)-(\d+)-(\d+)--(\d+)-(\d+)-(\d+)', filename)
+    year, month, day, hour, min, sec = (int(_) for _ in filename_regex.groups())
+    date = datetime.datetime(year, month, day, hour=hour, minute=min, second=sec)
+    plant_age = date - PLANT_START_DATE
+    # Normalize age between 0 and 1
+    target = plant_age.total_seconds() / PLANT_AGE_MULT
     return image, target
 
 
