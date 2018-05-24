d84ad10ab54d6c9059aaef8e7928d6251bf1a83c
ook-yoga <hugo.p.cmu@gmail.com>
2018-05-23 21:29:31 -0700

    action classes brainstorming

--- actions.py
+++ actions.py
@@ -1,3 +1,20 @@
+
+
+class Action:
+
+
+class Image(Action):
+
+class
+
+class Fan(Action):
+
+class Water(Action):
+
+class Drain(Action):
+
+class Light(Action):
+
 class Growing:
  # Class for a growing based event. This suggests that there should maybe be an "Event" class which planting and grwoing
 # inherit from
