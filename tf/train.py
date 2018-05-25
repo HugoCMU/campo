044c43effc4004b5bcb818ffc624957047bec586
ook-yoga <hugo.p.cmu@gmail.com>
2018-05-24 18:06:55 -0700

    debugging train input function

--- tf/train.py
+++ tf/train.py
@@ -10,46 +10,51 @@ data_dir = root_dir / 'data' / 'images_and_annotations'
 train_dir = data_dir / 'PSI_Tray031' / 'tv'
 test_dir = data_dir / 'PSI_Tray032' / 'tv'
 PLANT_START_DATE = datetime.datetime(2015, 12, 14, hour=12, minute=54, second=51)
-PLANT_AGE_MULT = 1.5 * 2592000 # Age of plant in seconds for normalizing
+PLANT_AGE_MULT = 1.5 * 2592000  # Age of plant in seconds for normalizing
 
 # feature encoder is taken from tf hub
-image_features = hub.image_embedding_column('plant-photo',
-                                            'https://tfhub.dev/google/imagenet/nasnet_large/feature_vector/1')
+image_features = hub.image_embedding_column('image',
+                                            'https://tfhub.dev/google/imagenet/resnet_v1_50/feature_vector/1')
 
 # Build model (regression model on top of image features)
-plant_model = tf.estimator.DNNRegressor(feature_columns=image_features,
+plant_model = tf.estimator.DNNRegressor(feature_columns=[image_features],
                                         hidden_units=[256, 128, 64])
 
 
-def parse_single_image(filename, train=True):
-    # Decode and convert image to appropriate type
-    image = tf.image.decode_png(tf.read_file(filename))
-    image = tf.image.convert_image_dtype(image, tf.float32)  # general image convention for modules
-    # Resize according to module requirements
-    image = tf.image.resize_images(image, [331, 331])
-    # Augmentation in training
-    if train:
-        image = tf.image.random_contrast(image)
-        image = tf.image.random_brightness(image)
-    # TODO: Use regex to extract target from filename
+def plant_age_from_filename(filename):
     # Sample filename: PSI_Tray031_2015-12-26--17-38-25_top.png
     filename_regex = re.search(r'(201\d)-(\d+)-(\d+)--(\d+)-(\d+)-(\d+)', filename)
     year, month, day, hour, min, sec = (int(_) for _ in filename_regex.groups())
     date = datetime.datetime(year, month, day, hour=hour, minute=min, second=sec)
     plant_age = date - PLANT_START_DATE
     # Normalize age between 0 and 1
-    target = plant_age.total_seconds() / PLANT_AGE_MULT
-    return image, target
+    return plant_age.total_seconds() / PLANT_AGE_MULT
 
 
-def input_fn(image_dir,
-             train=True,
-             shuffle_buffer=600,
-             num_epochs=20,
-             batch_size=32):
-    # Extract files
-    files = tf.data.Dataset.list_files(image_dir)
-    dataset = tf.data.TFRecordDataset(files)
+def age_gen_from_dir(image_dir):
+    for filename in image_dir:
+        yield plant_age_from_filename(filename)
+
+
+def parse_single_image(filename, target, train=True):
+    # Decode and convert image to appropriate type
+    image = tf.image.decode_png(tf.read_file(filename))
+    image = tf.image.convert_image_dtype(image, tf.float32)  # general image convention for modules
+    # Resize according to module requirements
+    image = tf.image.resize_images(image, [224, 224])
+    # Augmentation in training
+    if train:
+        pass
+        # image = tf.image.random_contrast(image)
+        # image = tf.image.random_brightness(image)
+    return {'image': image}, target
+
+
+def input_fn(image_dir, train=True, shuffle_buffer=10, num_epochs=4, batch_size=2):
+    # Create a dataset of (filename, plant_age) tuples
+    image_files = tf.data.Dataset.list_files(image_dir + '*.png')
+    plant_ages = tf.data.Dataset.from_generator(age_gen_from_dir, tf.float32)
+    dataset = tf.data.Dataset.zip((image_files, plant_ages))
 
     # Shuffle/Repeat data together for speed
     dataset = dataset.apply(
@@ -57,18 +62,16 @@ def input_fn(image_dir,
 
     # Map function will be different on train and evaluation
     dataset = dataset.apply(
-        tf.contrib.data.map_and_batch(lambda x: parse_single_image(x, train),
+        tf.contrib.data.map_and_batch(lambda filename, target: parse_single_image(filename, target, train),
                                       batch_size))
 
     # Use GPU prefetch to speed up training
-    dataset = dataset.apply(tf.contrib.data.prefetch_to_device('/gpu:0'))
+    # dataset = dataset.apply(tf.contrib.data.prefetch_to_device('/gpu:0'))
 
-    # Make iterator as final return type
+    # Make iterator, and yield the next batch
     iterator = dataset.make_one_shot_iterator()
     features = iterator.get_next()
-
-    # TODO:  Return must include feature vectors defined through hub encoder
-    return ({'plant-photo':}, features)
+    return features
 
 
 # Train and evaluate
