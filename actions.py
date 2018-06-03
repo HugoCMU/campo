220bd6bf8bd1cde2ca7c592a1a138c5ebcbe5b25
ook-yoga <hugo.p.cmu@gmail.com>
2018-06-03 11:54:16 -0700

    brainstorming action class and child classes

--- actions.py
+++ actions.py
@@ -1,24 +1,58 @@
+from pathlib import Path
+import datetime
+import pandas
 
 
 class Action:
+    # Directory locations for logging
+    root_dir = Path.cwd()
+    img_dir = root_dir / 'local' / 'images'
+    log_dir = root_dir / 'local' / 'logs'
+
+    def __init__(self, plant_ids=None):
+        if not plant_ids:
+            raise Exception('Action must have a plant id')
+        # There can be multiple plant id's, actions can affect multiple plants
+        self.plant_ids = plant_ids
+
+    def on(self):
+        raise NotImplementedError
+
+    def off(self):
+        raise NotImplementedError
+
+    def log(self):
+        time = datetime.datetime.now()
+        pass
+        # Create entry in plant_id csv file with action information
 
 
 class Image(Action):
+    name = 'image'
 
-class
+    # action: take image
+    # time taken, resolution, reference id to plant
+    # feed through aging network, get age
+
 
 class Fan(Action):
+    name = 'fan'
+
+    # boolean state: turn fan on or off
+    # action: turn fan on for X minutes
+    # turn fan on until temperature reaches something?
+
 
 class Water(Action):
+    name = 'water'
 
-class Drain(Action):
+    # turn on water pump for X minutes
+    # turn on water pump until humidity measures something?
+
 
 class Light(Action):
+    name = 'light'
 
-class Growing:
- # Class for a growing based event. This suggests that there should maybe be an "Event" class which planting and grwoing
-# inherit from
-
-class Planting:
-
-
+    # boolean state: turn light on or off
+    # type of light: vegetation vs flowering
+    # duration
--- plant.py
+++ plant.py
@@ -1,51 +1,15 @@
-from pathlib import Path
 import datetime
-import functools
-
-
-def log(func):
-    """
-    Log decorator creates log files for event functions
-    :param func:
-    :return:
-    """
-    @functools.wraps(func)
-    def _(*args, **kwargs):
-        time = datetime.datetime.now()
-        result = func(*args, **kwargs, time=time)
-        if not result:
-            raise Exception()
-        print(result + "  " + str(time))
-    return _
-
 
 class Plant:
-    
-    # Directory locations for logging
-    root_dir = Path.cwd()
-    img_dir = root_dir / 'local' / 'images'
-    log_dir = root_dir / 'local' / 'logs'
 
-    def __init__(self, name):
+    def __init__(self, name, soil_type='soil'):
         self.name = name
-        self.pot = None
-        self.soil = None
-        self.last_watered = None
+        self.soil_type = soil_type
 
-    @log
-    def water(self, duration, **kwargs):
-        self.last_watered = kwargs.get('time') + duration
-        return f'Watering {self.name} for {duration}'
-
-    @log
     def plant(self, pot, soil, **kwargs):
         self.pot = pot
         self.soil = soil
         return f'Planting {self.name} in {pot} with {soil}'
-
-    @log
-    def image(self):
-        pass
 
 
 if __name__ == '__main__':
