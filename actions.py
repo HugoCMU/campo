02dfbd54f7b7e74c28d6a8c084f6ad9aa5990d6a
ook-yoga <hugo.p.cmu@gmail.com>
2018-06-06 20:48:37 -0700

    RelayAction class instead of individual Action sub-classes

--- actions.py
+++ actions.py
@@ -2,7 +2,6 @@ import datetime
 import serial
 # Repo specific imports
 import util
-from campo import Campo
 
 
 class Action:
@@ -10,28 +9,10 @@ class Action:
     cols = ['name', 'time']
     name = '-'
 
-    # Serial communication defaults for Arduino (see .ino file)
-    port = '/dev/'
-    baud = 9600
-    serial_command_dict = {'pump_on': 'a',
-                           'pump_off': 'b',
-                           'vlight_on': 'c',
-                           'vlight_off': 'd',
-                           'flight_on': 'e',
-                           'flight_off': 'f',
-                           }
-
     def __init__(self, campo=None):
         assert campo, 'Action must include a campo'
         # There can be multiple plants per campo
         self.plants = list(campo.list_plants()['id'].values)
-
-    def serial_command(self, command):
-        com = self.serial_command_dict[command]
-        assert com, f'Serial command {com} not found'
-        with serial.Serial(self.port, self.baud) as ser:
-            ser.write(com.encode())
-        self.log(relay=command)
 
     @util.timer
     def log(self, **kwargs):
@@ -42,32 +23,46 @@ class Action:
             util.save_row(plant, new_row_dict)
 
 
+class RelayAction(Action):
+    # Serial communication defaults for Arduino (see .ino file)
+    port = '/dev/'
+    baud = 9600
+    available_relays = ['pump', 'vlight', 'flight']
+    serial_command_dict = {'pump_on': 'a',
+                           'pump_off': 'b',
+                           'vlight_on': 'c',
+                           'vlight_off': 'd',
+                           'flight_on': 'e',
+                           'flight_off': 'f',
+                           }
+
+    def __init__(self, campo=None):
+        super(self, RelayAction).__init__(campo)
+        self.on_timer = None
+        self.timedelta = None
+
+    def on(self, relay=None):
+        assert relay in self.available_relays, f'Relay {relay} not in list of available relays'
+        self.on_timer = datetime.datetime.now()
+        self.serial_command(relay + '_on', state='on')
+
+    def off(self, relay=None):
+        assert relay in self.available_relays, f'Relay {relay} not in list of available relays'
+        self.timedelta = self.on_timer - datetime.datetime.now()
+        self.serial_command(relay + '_off', state='off')
+
+    def serial_command(self, command, **kwargs):
+        com = self.serial_command_dict[command]
+        assert com, f'Serial command {com} not found'
+        with serial.Serial(self.port, self.baud) as ser:
+            ser.write(com.encode())
+        kwargs['serial_command'] = command
+        self.log(kwargs)
+
+
 class Image(Action):
     name = 'image'
 
     # action: take image
     # time taken, resolution, reference id to plant
     # feed through aging network, get age
-
-
-class Fan(Action):
-    name = 'fan'
-
-    # boolean state: turn fan on or off
-    # action: turn fan on for X minutes
-    # turn fan on until temperature reaches something?
-
-
-class Water(Action):
-    name = 'water'
-
-    # turn on water pump for X minutes
-    # turn on water pump until humidity measures something?
-
-
-class Light(Action):
-    name = 'light'
-
-    # boolean state: turn light on or off
-    # type of light: vegetation vs flowering
-    # duration
