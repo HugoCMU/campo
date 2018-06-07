9db0a7e31fd5e50afe79e233685d3bb95cf0704d
ook-yoga <hugo.p.cmu@gmail.com>
2018-06-06 18:11:50 -0700

    Serial communication to arduino

--- actions.py
+++ actions.py
@@ -1,4 +1,5 @@
 import datetime
+import serial
 # Repo specific imports
 import util
 from campo import Campo
@@ -9,10 +10,28 @@ class Action:
     cols = ['name', 'time']
     name = '-'
 
+    # Serial communication defaults for Arduino (see .ino file)
+    port = '/dev/'
+    baud = 9600
+    serial_command_dict = {'pump_on': 'a',
+                           'pump_off': 'b',
+                           'vlight_on': 'c',
+                           'vlight_off': 'd',
+                           'flight_on': 'e',
+                           'flight_off': 'f',
+                           }
+
     def __init__(self, campo=None):
         assert campo, 'Action must include a campo'
         # There can be multiple plants per campo
         self.plants = list(campo.list_plants()['id'].values)
+
+    def serial_command(self, command):
+        com = self.serial_command_dict[command]
+        assert com, f'Serial command {com} not found'
+        with serial.Serial(self.port, self.baud) as ser:
+            ser.write(com.encode())
+        self.log(relay=command)
 
     @util.timer
     def log(self, **kwargs):
