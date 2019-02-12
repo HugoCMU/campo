8890f61a630851f9153dd9d9e056029056030eb0
HugoCMU <hugo.p.cmu@gmail.com>
2019-02-12 08:59:35 -0800

    Debugging gym env.

--- envs/campo.py
+++ envs/campo.py
@@ -1,10 +1,14 @@
+"""Gym environment for campo RL algo."""
 import collections
+import logging
 import os
 import gym
 import time
 
 # Manually import realsense module
 import importlib.util
+
+import pdb
 
 REALSENSE_PATH = os.path.abspath(
     os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../realsense-tcp-module/cameras.py'))
@@ -17,7 +21,7 @@ class CampoEnv(gym.Env):
     """Gym environment for campo."""
 
     def __init__(self):
-        # super().__init__()
+        super().__init__()
         self.camera = realsense_module.RealSense(tcp_ip='127.0.0.1', tcp_port=50200)
         time.sleep(1)  # Give camera some time to load data
 
@@ -30,17 +34,29 @@ class CampoEnv(gym.Env):
         obs['color_intrinsics'] = self.camera.color_intr
         obs['depth_intrinsics'] = self.camera.depth_intr
         obs['depth2color_extrinsic'] = self.camera.depth2color_extr
+        pdb.set_trace()
+        logging.debug('STEP')
+        logging.debug('timestamp %s' % self.camera.timestamp)
+        logging.debug('color image   %s   %s' % (self.camera.color_im.dtype, self.camera.color_im.shape))
+        logging.debug('depth image   %s   %s' % (self.camera.depth_im.dtype, self.camera.depth_im.shape))
+        return obs
 
     def step(self, action):
-        pass
+        del action
+        is_done = False
+        obs = self._get_obs()
+        reward = 1.0
+        pdb.set_trace()
+        return obs, reward, is_done, {}
 
     def reset(self):
-        pass
-
-    def render(self, mode='human', close=False):
-        pass
+        return self._get_obs()
 
 
 if __name__ == '__main__':
+    logging.getLogger(__name__).setLevel(logging.DEBUG)
+    pdb.set_trace()
     testcampo = CampoEnv()
     testcampo._get_obs()
+    testcampo.reset()
+    testcampo.step(None)
