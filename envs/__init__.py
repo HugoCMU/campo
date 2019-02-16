151503a4ee86a5771ce7645f7cc33bccb9b12314
HugoCMU <hugo.p.cmu@gmail.com>
2019-02-16 08:58:04 -0800

    Added servos using dxl_py.

--- envs/__init__.py
+++ envs/__init__.py
@@ -3,4 +3,5 @@ from gym.envs.registration import register
 register(
     id='campo-v0',
     entry_point='campo.envs:CampoEnv',
+    max_steps=10,
 )
--- envs/campo.py
+++ envs/campo.py
@@ -4,6 +4,8 @@ import logging
 import os
 import gym
 import time
+import numpy as np
+from dxl_py.servos import Servos
 
 # Manually import realsense module
 import importlib.util
@@ -16,6 +18,28 @@ spec = importlib.util.spec_from_file_location("realsense-tcp-module.cameras", RE
 realsense_module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(realsense_module)
 
+# Servo config dictionary contains individual per-servo information
+SERVO_CONFIG = {
+    # 'servo_1': {
+    #     'id': 1,
+    #     'model': 'mx',
+    #     'min': 0,
+    #     'max': 4095,
+    # },
+    'servo_2': {
+        'id': 2,
+        'model': 'mx',
+        'min': 0,
+        'max': 4095,
+    },
+    'servo_3': {
+        'id': 3,
+        'model': 'mx',
+        'min': 0,
+        'max': 4095,
+    },
+}
+
 
 class CampoEnv(gym.Env):
     """Gym environment for campo."""
@@ -24,6 +48,17 @@ class CampoEnv(gym.Env):
         super().__init__()
         self.camera = realsense_module.RealSense(tcp_ip='127.0.0.1', tcp_port=50200)
         time.sleep(1)  # Give camera some time to load data
+        self.servos = Servos(
+            device_name='/dev/ttyUSB0',
+            baudrate=1000000,
+            protocol_version=2.0,
+            action_bounds=[-1.0, 1.0],
+            config=SERVO_CONFIG)
+        self.reset_pose = {
+            # 'servo_1': np.random.uniform(low=-1.0, high=1.0),
+            'servo_2': np.random.uniform(low=-1.0, high=1.0),
+            'servo_3': np.random.uniform(low=-1.0, high=1.0),
+        }
 
     def _get_obs(self):
         obs = collections.OrderedDict()
@@ -34,29 +69,43 @@ class CampoEnv(gym.Env):
         obs['color_intrinsics'] = self.camera.color_intr
         obs['depth_intrinsics'] = self.camera.depth_intr
         obs['depth2color_extrinsic'] = self.camera.depth2color_extr
-        pdb.set_trace()
         logging.debug('STEP')
         logging.debug('timestamp %s' % self.camera.timestamp)
         logging.debug('color image   %s   %s' % (self.camera.color_im.dtype, self.camera.color_im.shape))
         logging.debug('depth image   %s   %s' % (self.camera.depth_im.dtype, self.camera.depth_im.shape))
+        # obs['servo_1'] = self.servos.get(['servo_1'])
+        obs['servo_2'] = self.servos.get(['servo_2'])
+        obs['servo_3'] = self.servos.get(['servo_3'])
         return obs
 
     def step(self, action):
-        del action
-        is_done = False
-        obs = self._get_obs()
-        reward = 1.0
         pdb.set_trace()
-        return obs, reward, is_done, {}
+        self.servos.set({'servo_2': action[0]})
+        obs_dict = self._get_obs()
+        return obs_dict['depth_image'], self.get_reward(), self.is_done(), {}
+
+    def get_reward(self):
+        # pos = self.servos.get(['servo_1', 'servo_2', 'servo_3'])
+        pos = self.servos.get(['servo_2', 'servo_3'])
+        reward = -abs(pos[0] - pos[1])
+        return reward
+
+    def is_done(self):
+        return False
 
     def reset(self):
+        self.reset_pose = {
+            # 'servo_1': np.random.uniform(low=-1.0, high=1.0),
+            'servo_2': np.random.uniform(low=-1.0, high=1.0),
+            'servo_3': np.random.uniform(low=-1.0, high=1.0),
+        }
+        self.servos.set(self.reset_pose)
         return self._get_obs()
 
 
 if __name__ == '__main__':
     logging.getLogger(__name__).setLevel(logging.DEBUG)
-    pdb.set_trace()
     testcampo = CampoEnv()
     testcampo._get_obs()
     testcampo.reset()
-    testcampo.step(None)
+    testcampo.step([-1.0])
